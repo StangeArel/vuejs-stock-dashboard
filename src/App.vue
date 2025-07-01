fcae430a6d1381edba104115c220b6cdd326af88
Valeriya Stange <valeriya.stange@gmail.com>
2025-07-01 11:24:23 +0200

    feat: create first component and fix errors

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app"></div>
+  <MyCard>
+    <h1>Überschrift</h1>
+    <p>Dies ist ein Beispiel</p>
+  </MyCard>
 </template>
 
 <script>
+import MyCard from "./components/MyCard";
+
 export default {
   name: 'App',
+  components: { MyCard }
 }
 </script>
+
 
 <style>
 body {
@@ -14,8 +21,10 @@ body {
 }
 
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   min-height: 100vw;
   width: 100vh;
+  padding: 100px;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/MyCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="myCard">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyCard'
+}
+</script>
+
+<style scoped>
+.myCard {
+    background-color: #011f35;
+    border-radius: 16px;
+    padding: 24px 32px;
+    border: 3px solid red;
+}
+</style>
